8a7b68004091d723f7cead8d43598b1625f6a9e3
Aaron Rodi <metalhead5209@gmail.com>
2022-09-03 16:31:14 -0500

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Home from './pages/home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <h1>Home Skillet</h1>
+  )
+}
+
+export default home
